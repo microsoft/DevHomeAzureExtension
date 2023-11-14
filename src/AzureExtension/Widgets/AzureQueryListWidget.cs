7cbc85bced2be1740752cc540bae72e09e60383f
Kristen Schau <47155823+krschau@users.noreply.github.com>
2023-11-14 13:04:27 -0500

    remove todos

--- src/AzureExtension/Widgets/AzureQueryListWidget.cs
+++ src/AzureExtension/Widgets/AzureQueryListWidget.cs
@@ -168,9 +168,6 @@ internal class AzureQueryListWidget : AzureWidget
 
                 // The DataManager log will have detailed exception info, use the short message.
                 Log.Logger()?.ReportError(Name, ShortId, $"Data update failed. {e.Context.QueryId} {e.Context.ErrorMessage}");
-
-                // TODO: Display error to user however design deems appropriate.
-                // https://github.com/microsoft/DevHomeADOExtension/issues/50
                 return;
             }
 
