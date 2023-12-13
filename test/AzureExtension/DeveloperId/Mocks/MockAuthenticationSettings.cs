d79b2eb9631585b20d9aec3d9537e4a96b9397fb
ssparach <128866445+ssparach@users.noreply.github.com>
2023-12-12 16:02:59 -0800

    [DeveloperId]: Add unit tests for feature area (#76)
    * test setup for DevId and addition of simple unit test
    
    * addition of unit tests
    
    * address PR comments
    
    * nit: fix name of unit test

---
+++ test/AzureExtension/DeveloperId/Mocks/MockAuthenticationSettings.cs
@@ -0,0 +1,19 @@
+﻿// Copyright (c) Microsoft Corporation and Contributors
+// Licensed under the MIT license.
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using DevHomeAzureExtension.DeveloperId;
+
+namespace AzureExtension.Test.DeveloperId.Mocks;
+
+public class MockAuthenticationSettings : IAuthenticationSettings
+{
+    public void InitializeSettings()
+    {
+        // no settings to initalize for testing
+    }
+}
