621a6a677203279161a5dcc7cc625e476616b267
Darren Hoehna <dahoehna@microsoft.com>
2024-02-09 16:31:36 -0800

    Adding comments

--- src/AzureExtension/Helpers/AzureRepositoryHierarchy.cs
+++ src/AzureExtension/Helpers/AzureRepositoryHierarchy.cs
@@ -14,15 +14,24 @@ using Organization = Microsoft.VisualStudio.Services.Account.Account;
 
 namespace AzureExtension.Helpers;
 
+/// <summary>
+/// Handles the hierarchy between organizations and projects.  Handles querying for both as well.
+/// </summary>
 public class AzureRepositoryHierarchy
 {
-    private readonly DeveloperId _developerId;
-
     private readonly ConcurrentDictionary<Organization, List<TeamProjectReference>> _organizationsAndProjects;
 
-    private Task<List<Organization>>? _queryOrganizationsTask;
+    /// <summary>
+    /// Used to keep track of multiple Get requests to make sure work isn't duplicated.
+    /// </summary>
+    private readonly ConcurrentDictionary<Organization, Task<IPagedList<TeamProjectReference>>> _organizationsAndProjectTask;
 
-    private ConcurrentDictionary<Organization, Task<IPagedList<TeamProjectReference>>> _organizationsAndProjectTask;
+    private readonly DeveloperId _developerId;
+
+    /// <summary>
+    /// Used to keep track of all GetOrganization requests to make sure work isn't duplicated.
+    /// </summary>
+    private Task<List<Organization>>? _queryOrganizationsTask;
 
     /// <summary>
     /// Initializes a new instance of the <see cref="AzureRepositoryHierarchy"/> class.
@@ -41,6 +50,10 @@ public class AzureRepositoryHierarchy
         _organizationsAndProjectTask = new ConcurrentDictionary<Organization, Task<IPagedList<TeamProjectReference>>>();
     }
 
+    /// <summary>
+    /// Get all organizations the is apart of.
+    /// </summary>
+    /// <returns>A list of all organizations.</returns>
     public async Task<List<Organization>> GetOrganizationsAsync()
     {
         // if something is running the query, wait for it to finish.
@@ -64,6 +77,11 @@ public class AzureRepositoryHierarchy
         return _organizationsAndProjects.Keys.ToList();
     }
 
+    /// <summary>
+    /// Get all projects the user is apart of.
+    /// </summary>
+    /// <param name="organization">Filters down the returned list to those under organization</param>
+    /// <returns>A list of projects.</returns>
     public async Task<List<TeamProjectReference>> GetProjectsAsync(Organization organization)
     {
         // if something is running the query, wait for it to finish.
@@ -89,6 +107,14 @@ public class AzureRepositoryHierarchy
         return _organizationsAndProjects[organization];
     }
 
+    /// <summary>
+    /// Contacts the server to get all organizations the user is apart of.
+    /// </summary>
+    /// <returns>A list of organizations.</returns>
+    /// <remarks>
+    /// The returned list does include disabled organizations.
+    /// _queryOrganizationsTask is set here.
+    /// </remarks>
     private List<Organization> QueryForOrganizations()
     {
         // Establish a connection to get all organizations.
@@ -118,6 +144,14 @@ public class AzureRepositoryHierarchy
         }
     }
 
+    /// <summary>
+    /// Contacts the server to get all projects in an organization.
+    /// </summary>
+    /// <param name="organization">PRojects are returned only for this organization.</param>
+    /// <returns>A list of projects.</returns>
+    /// <remarks>
+    /// the Task to get the projects is added to _organizationsAndProjectTask.
+    /// </remarks>
     private List<TeamProjectReference> QueryForProjects(Organization organization)
     {
         try
--- src/AzureExtension/Providers/RepositoryProvider.cs
+++ src/AzureExtension/Providers/RepositoryProvider.cs
@@ -23,6 +23,9 @@ namespace DevHomeAzureExtension.Providers;
 
 public class RepositoryProvider : IRepositoryProvider, IRepositoryProvider2
 {
+    /// <summary>
+    /// Used for singleton instance.
+    /// </summary>
     private static readonly object _constructorLock = new();
 
     private static RepositoryProvider? _repositoryProvider;
@@ -43,10 +46,6 @@ public class RepositoryProvider : IRepositoryProvider, IRepositoryProvider2
 
     private string _selectedProject;
 
-    private bool _isFirstQuery;
-
-    private CancellationTokenSource _tokenSource;
-
     public string DisplayName => Resources.GetResource(@"RepositoryProviderDisplayName");
 
     public IRandomAccessStreamReference Icon
@@ -56,16 +55,17 @@ public class RepositoryProvider : IRepositoryProvider, IRepositoryProvider2
 
     string[] IRepositoryProvider2.GetSearchFieldNames => new string[3] { _server, _organization, _project };
 
+    /// <summary>
+    /// On first query, get repos based on the PR date.  All other requests won't check for PRs.
+    /// </summary>
     private bool _shouldIgnorePRDate;
 
     public RepositoryProvider(IRandomAccessStreamReference icon)
     {
         Icon = icon;
-        _isFirstQuery = true;
         _selectedServer = string.Empty;
         _selectedOrganization = string.Empty;
         _selectedProject = string.Empty;
-        _tokenSource = new();
     }
 
     public RepositoryProvider()
@@ -108,15 +108,20 @@ public class RepositoryProvider : IRepositoryProvider, IRepositoryProvider2
     }
 
     /// <summary>
-    /// Gets all repos from the project that the user did a pull request into
+    /// Get all repositories under a porject.
     /// </summary>
-    private List<IRepository> GetRepos(VssConnection connection, TeamProjectReference project, GitPullRequestSearchCriteria criteria, bool shouldIgnorePRDate)
+    /// <param name="organizationConnection">The connection to the organization.</param>
+    /// <param name="project">The project to get the repos for</param>
+    /// <param name="criteria">Holds the user ID.</param>
+    /// <param name="shouldIgnorePRDate">If criteria should be used or not.</param>
+    /// <returns>A list of IRepository's.</returns>
+    private List<IRepository> GetRepos(VssConnection organizationConnection, TeamProjectReference project, GitPullRequestSearchCriteria criteria, bool shouldIgnorePRDate)
     {
         Log.Logger()?.ReportInfo("DevHomeRepository", $"Getting all repos for {project.Name}");
         try
         {
-            var gitClient = connection.GetClient<GitHttpClient>();
-            var repos = gitClient.GetRepositoriesAsync(project.Id, true, true).Result.OrderBy(x => x.Name);
+            var gitClient = organizationConnection.GetClient<GitHttpClient>();
+            var repos = gitClient.GetRepositoriesAsync(project.Id, false, false).Result.OrderBy(x => x.Name);
 
             var reposToReturn = new List<IRepository>();
             foreach (var repo in repos)
@@ -151,6 +156,11 @@ public class RepositoryProvider : IRepositoryProvider, IRepositoryProvider2
         }
     }
 
+    /// <summary>
+    /// Get repositories for the developer.
+    /// </summary>
+    /// <param name="developerId">The account to use to get repositories.</param>
+    /// <returns>A list of repositories the user has access to.</returns>
     public IAsyncOperation<RepositoriesResult> GetRepositoriesAsync(IDeveloperId developerId)
     {
         if (developerId is not DeveloperId.DeveloperId azureDeveloperId)
@@ -167,6 +177,7 @@ public class RepositoryProvider : IRepositoryProvider, IRepositoryProvider2
         var server = string.Empty;
         var organizations = _azureHierarchy.GetOrganizationsAsync().Result;
 #pragma warning disable CA1309 // Use ordinal string comparison
+        // Try to find any organizations with the modern URL format.
         var defaultOrg = organizations.FirstOrDefault(x => x.AccountName.Equals(_defaultSearchServerName));
 #pragma warning restore CA1309 // Use ordinal string comparison
         if (defaultOrg != null)
@@ -349,25 +360,14 @@ public class RepositoryProvider : IRepositoryProvider, IRepositoryProvider2
 
     public IAsyncOperation<RepositoriesResult> GetRepositoriesAsync(IReadOnlyDictionary<string, string> searchTerms, IDeveloperId developerId)
     {
-        if (!_isFirstQuery)
-        {
-            _tokenSource.Cancel();
-            _tokenSource = new CancellationTokenSource();
-        }
-        else
-        {
-            _isFirstQuery = false;
-        }
-
+        // First query will find repos only if the user made a PR.
         var shouldIgnorePRDateOld = _shouldIgnorePRDate;
         _shouldIgnorePRDate = true;
-        try
-        {
-            return Task.Run(
-            () =>
+        return Task.Run(() =>
         {
             try
             {
+                // Store the search terms.
                 var serverToUse = searchTerms.ContainsKey(_server) ? searchTerms[_server] : string.Empty;
                 _selectedServer = serverToUse;
 
@@ -415,6 +415,7 @@ public class RepositoryProvider : IRepositoryProvider, IRepositoryProvider2
 
                 Dictionary<Organization, List<TeamProjectReference>> organizationsAndProjects = new Dictionary<Organization, List<TeamProjectReference>>();
 
+                // Establish a connection between organizations and their projects.
                 Parallel.ForEach(organizations, options, orgnization =>
                 {
                     var projects = _azureHierarchy.GetProjectsAsync(orgnization).Result;
@@ -461,24 +462,25 @@ public class RepositoryProvider : IRepositoryProvider, IRepositoryProvider2
                 Providers.Log.Logger()?.ReportError("DevHomeRepository", ex);
                 return new RepositoriesResult(new List<IRepository>());
             }
-        },
-            _tokenSource.Token).AsAsyncOperation();
-        }
-        catch (Exception ex)
-        {
-            return Task.Run(() =>
-            {
-                Providers.Log.Logger()?.ReportError("DevHomeRepository", ex);
-                return new RepositoriesResult(new List<IRepository>());
-            }).AsAsyncOperation();
-        }
+        }).AsAsyncOperation();
     }
 
+    /// <summary>
+    /// Returns a list of field names this will accept as search input.
+    /// </summary>
+    /// <returns>A readonly list of search field names.</returns>
     public IReadOnlyList<string> GetSearchFieldNames()
     {
         return new List<string> { _server, _organization, _project };
     }
 
+    /// <summary>
+    /// Gets a list of values that work with the given input.
+    /// </summary>
+    /// <param name="fieldName">Results will be for this search field.</param>
+    /// <param name="fieldValues">The inputs used to filter the results.</param>
+    /// <param name="developerId">Used to access private org and project information.</param>
+    /// <returns>A list of all values for fieldName that makes snes given the input.</returns>
     public IAsyncOperation<IList<string>> GetValuesForField(string fieldName, IReadOnlyDictionary<string, string> fieldValues, IDeveloperId developerId)
     {
         // Get access token for ADO API calls.
@@ -504,18 +506,21 @@ public class RepositoryProvider : IRepositoryProvider, IRepositoryProvider2
 #pragma warning disable CA1309 // Use ordinal string comparison.  Make sure names match linguisticly.
             if (fieldName.Equals(_organization))
             {
+                // Requesting organizations and an organization is given.
                 var organizations = _azureHierarchy.GetOrganizationsAsync().Result;
                 if (!string.IsNullOrEmpty(organizationToUse))
                 {
                     organizations = organizations.Where(x => x.AccountName.Equals(organizationToUse)).OrderBy(x => x.AccountName).ToList();
                 }
 
+                // If no projects are in the input, return the organizations.
                 if (string.IsNullOrEmpty(projectToUse))
                 {
                     return organizations.Select(x => x.AccountName).Order().ToList() as IList<string>;
                 }
                 else
                 {
+                    // Find all organizations with the given project name.
                     var organizationsToReturn = new List<string>();
                     foreach (var organization in organizations)
                     {
@@ -606,17 +611,27 @@ public class RepositoryProvider : IRepositoryProvider, IRepositoryProvider2
 #pragma warning restore CA1309 // Use ordinal string comparison
     }
 
+    /// <summary>
+    /// Used to return terms used for a default search.
+    /// In this case server is dev.azure.com if any organizations use the modern URL format.  Otherwise it is the old URL format.
+    /// Organization is the first one found.
+    /// </summary>
+    /// <param name="field">The search field to search for.</param>
+    /// <param name="developerId">Used to get server and organization information.</param>
+    /// <returns>A default search value.</returns>
     public string GetDefaultValueFor(string field, IDeveloperId developerId)
     {
         if (developerId is not DeveloperId.DeveloperId azureDeveloperId)
         {
-            throw new NotSupportedException("Authenticated user is not an azure developer id");
+            Providers.Log.Logger()?.ReportError("DevHomeRepository", "Authenticated user is not an azure developer id");
+            return string.Empty;
         }
 
         _azureHierarchy ??= new AzureRepositoryHierarchy(azureDeveloperId);
 
         var organizations = _azureHierarchy.GetOrganizationsAsync().Result;
 
+        // Get a default server name.
         if (field.Equals(_server, StringComparison.OrdinalIgnoreCase))
         {
             if (organizations.FirstOrDefault(x => x.AccountUri.OriginalString.Contains("dev.azure.com")) != null)
@@ -625,11 +640,19 @@ public class RepositoryProvider : IRepositoryProvider, IRepositoryProvider2
             }
             else
             {
-                var organization = organizations[0];
-                return $"{organization.AccountName}.visualstudio.com";
+                if (organizations.Count != 0)
+                {
+                    var organization = organizations[0];
+                    return $"{organization.AccountName}.visualstudio.com";
+                }
+                else
+                {
+                    return string.Empty;
+                }
             }
         }
 
+        // get the default organization
         if (field.Equals(_organization, StringComparison.OrdinalIgnoreCase))
         {
             var maybeNewOrganization = organizations.FirstOrDefault(x => x.AccountUri.OriginalString.Contains("dev.azure.com", StringComparison.OrdinalIgnoreCase));
@@ -639,7 +662,14 @@ public class RepositoryProvider : IRepositoryProvider, IRepositoryProvider2
             }
             else
             {
-                return organizations[0].AccountName;
+                if (organizations.Count != 0)
+                {
+                    return organizations[0].AccountName;
+                }
+                else
+                {
+                    return string.Empty;
+                }
             }
         }
 
