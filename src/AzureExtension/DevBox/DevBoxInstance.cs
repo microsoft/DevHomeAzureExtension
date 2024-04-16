4f3bfede9368f3710a37f1e877a750f34fbb7f43
Huzaifa Danish <Huzaifa.Danish@microsoft.com>
2024-04-16 15:00:29 -0700

    Dev Box - Adding customization support (#152)
    Co-authored-by: Huzaifa Danish <modanish@microsoft.com>

--- src/AzureExtension/DevBox/DevBoxInstance.cs
+++ src/AzureExtension/DevBox/DevBoxInstance.cs
@@ -1,8 +1,10 @@
 ﻿// Copyright (c) Microsoft Corporation.
 // Licensed under the MIT License.
 
+using System;
 using System.Diagnostics;
 using System.Runtime.InteropServices.WindowsRuntime;
+using System.Text;
 using System.Text.Json;
 using AzureExtension.Contracts;
 using AzureExtension.DevBox.DevBoxJsonToCsClasses;
@@ -133,7 +135,8 @@ public class DevBoxInstance : IComputeSystem
     }
 
     public ComputeSystemOperations SupportedOperations =>
-        ComputeSystemOperations.Start | ComputeSystemOperations.ShutDown | ComputeSystemOperations.Delete | ComputeSystemOperations.Restart;
+        ComputeSystemOperations.Start | ComputeSystemOperations.ShutDown | ComputeSystemOperations.Delete |
+        ComputeSystemOperations.Restart | ComputeSystemOperations.ApplyConfiguration;
 
     public string SupplementalDisplayName => $"{Resources.GetResource(SupplementalDisplayNamePrefix)}: {DevBoxState.ProjectName}";
 
@@ -484,6 +487,12 @@ public class DevBoxInstance : IComputeSystem
         }).AsAsyncOperation();
     }
 
+    public IApplyConfigurationOperation CreateApplyConfigurationOperation(string configuration)
+    {
+        var taskAPI = $"{DevBoxState.Uri}{Constants.CustomizationAPI}{DateTime.Now.ToFileTimeUtc()}?{Constants.APIVersion}";
+        return new WingetConfigWrapper(configuration, taskAPI, _devBoxManagementService, AssociatedDeveloperId, _log);
+    }
+
     // Unsupported operations
     public IAsyncOperation<ComputeSystemOperationResult> RevertSnapshotAsync(string options)
     {
@@ -540,9 +549,4 @@ public class DevBoxInstance : IComputeSystem
             return new ComputeSystemOperationResult(new NotImplementedException(), Resources.GetResource(Constants.DevBoxMethodNotImplementedKey), "Method not implemented");
         }).AsAsyncOperation();
     }
-
-    // Apply configuration isn't supported yet for Dev Boxes. This functionality will be created before the feature is released at build.
-    // Dev Home should not call this method and should use the Supported Operations to determine what operations are available.
-    // Currently, the supported operations are Start, Shutdown, Restart, and Delete.
-    public IApplyConfigurationOperation CreateApplyConfigurationOperation(string configuration) => throw new NotImplementedException();
 }
---
+++ src/AzureExtension/DevBox/Helpers/TaskJSONToCSClasses.cs
@@ -0,0 +1,81 @@
+﻿// Copyright (c) Microsoft Corporation.
+// Licensed under the MIT License.
+
+namespace AzureExtension.DevBox.Helpers;
+
+// Example of a task JSON response that will be deserialized into the BaseClass class
+//    {
+//  "tasks": [
+//    {
+//      "name": "winget",
+//      "parameters": {
+//        "inlineConfigurationBase64": ""
+//      },
+//      "runAs": "User",
+//      "id": "d473decc-0e0e-4452-9a17-f93e4fe5fc2b",
+//      "logUri": ",
+//      "status": "Succeeded",
+//      "startTime": "2024-04-09T20:09:03.0135721+00:00",
+//      "endTime": "2024-04-09T20:09:25.047521+00:00"
+//    },
+//    {
+//    "name": "winget",
+//      "parameters": {
+//        "inlineConfigurationBase64": ""
+//      },
+//      "runAs": "User",
+//      "id": "760eb6f5-fb72-452f-9b3c-d0c189b78c00",
+//      "logUri": "",
+//      "status": "Succeeded",
+//      "startTime": "2024-04-09T20:09:25.8467306+00:00",
+//      "endTime": "2024-04-09T20:09:38.5302446+00:00"
+//    }
+//  ],
+//  "uri": "",
+//  "name": "AzureExt133571668748121674",
+//  "status": "Running",
+//  "startTime": "2024-04-09T20:09:03.0136738+00:00"
+// }
+//
+/// <summary>
+/// Represents the classes for the customization task JSON response.
+/// </summary>
+public class TaskJSONToCSClasses
+{
+    public class BaseClass
+    {
+        public List<TaskItem> Tasks { get; set; } = new();
+
+        public string Uri { get; set; } = string.Empty;
+
+        public string Name { get; set; } = string.Empty;
+
+        public string Status { get; set; } = string.Empty;
+
+        public string StartTime { get; set; } = string.Empty;
+    }
+
+    public class TaskItem
+    {
+        public string Name { get; set; } = string.Empty;
+
+        public Parameters? Parameters { get; set; }
+
+        public string RunAs { get; set; } = string.Empty;
+
+        public string Id { get; set; } = string.Empty;
+
+        public string LogUri { get; set; } = string.Empty;
+
+        public string Status { get; set; } = string.Empty;
+
+        public string StartTime { get; set; } = string.Empty;
+
+        public string EndTime { get; set; } = string.Empty;
+    }
+
+    public class Parameters
+    {
+        public string InlineConfigurationBase64 { get; set; } = string.Empty;
+    }
+}
