0f3c44c000157ade88d9055aff5f2d02d7d70de6
Huzaifa Danish <Huzaifa.Danish@microsoft.com>
2024-05-08 10:43:50 -0700

    Handle "Waiting for user session" Dev Box config status; Fix slow loading (#168)
    Co-authored-by: Huzaifa Danish <modanish@microsoft.com>

--- src/AzureExtension/DevBox/DevBoxProvider.cs
+++ src/AzureExtension/DevBox/DevBoxProvider.cs
@@ -142,8 +142,9 @@ public class DevBoxProvider : IComputeSystemProvider
 
         // update the cache every time we retrieve new Dev Boxes. This is used so in the creation flow we don't need
         // to retrieve the Dev Boxes again if the user already retrieved them in the environments page in Dev Home
-        _cachedDevBoxesMap[GetUniqueDeveloperId(developerId)] = devBoxes.ToList();
-        return _cachedDevBoxesMap[GetUniqueDeveloperId(developerId)];
+        var uniqueUserId = GetUniqueDeveloperId(developerId);
+        _cachedDevBoxesMap[uniqueUserId] = devBoxes.ToList();
+        return _cachedDevBoxesMap[uniqueUserId];
     }
 
     /// <summary>
@@ -222,6 +223,8 @@ public class DevBoxProvider : IComputeSystemProvider
                 {
                     _log.Information($"No cached Dev Boxes found for {developerId.LoginId}, getting new Dev Boxes");
                     await GetDevBoxesAsync(developerId);
+                    _devBoxProjectAndPoolsMap[uniqueId] = await
+                        _devBoxManagementService.GetAllProjectsToPoolsMappingAsync(_devBoxProjectsMap[uniqueId]!, developerId);
                 }
 
                 return new ComputeSystemAdaptiveCardResult(new CreationAdaptiveCardSession(_devBoxProjectAndPoolsMap[uniqueId], _cachedDevBoxesMap[uniqueId]));
@@ -257,13 +260,6 @@ public class DevBoxProvider : IComputeSystemProvider
         var requestContent = new StringContent(Constants.ARGQuery, Encoding.UTF8, "application/json");
         var result = await _devBoxManagementService.HttpsRequestToManagementPlane(new Uri(Constants.ARGQueryAPI), developerId, HttpMethod.Post, requestContent);
         var devBoxProjects = JsonSerializer.Deserialize<DevBoxProjects>(result.JsonResponseRoot.ToString(), Constants.JsonOptions);
-
-        // Get Associated pools the first time we get the projects
-        if (!_devBoxProjectAndPoolsMap.TryGetValue(uniqueUserId, out var _))
-        {
-            _log.Information($"Found no cached pools for all projects for {developerId.LoginId}, retrieving pools");
-            _devBoxProjectAndPoolsMap[uniqueUserId] = await _devBoxManagementService.GetAllProjectsToPoolsMappingAsync(devBoxProjects!, developerId);
-        }
 
         _devBoxProjectsMap.Add(uniqueUserId, devBoxProjects!);
         return devBoxProjects!;
