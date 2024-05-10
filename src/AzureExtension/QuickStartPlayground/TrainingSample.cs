76180799f498925ae159a7fcdc3c4b515ff49fba
Manodasan Wignarajah <mawign@microsoft.com>
2024-05-09 19:49:17 -0700

    Merged PR 10740379: Adding extension side of QuickStart Playground experimental feature

---
+++ src/AzureExtension/QuickStartPlayground/TrainingSample.cs
@@ -0,0 +1,52 @@
+﻿// Copyright (c) Microsoft Corporation.
+// Licensed under the MIT License.
+
+using System.Text.Json.Serialization;
+
+namespace AzureExtension.QuickStartPlayground;
+
+public sealed class TrainingSample
+{
+    public required string Name
+    {
+        get; set;
+    }
+
+    public required string Codespaces
+    {
+        get; set;
+    }
+
+    public required string Prompt
+    {
+        get; set;
+    }
+
+    public required string Code
+    {
+        get; set;
+    }
+
+    public required string Readme
+    {
+        get; set;
+    }
+
+    public required ReadOnlyMemory<float> Embedding
+    {
+        get; set;
+    }
+
+    public required string Language
+    {
+        get; set;
+    }
+}
+
+#pragma warning disable SA1402 // File may only contain a single type
+[JsonSourceGenerationOptions(PropertyNameCaseInsensitive = true)]
+[JsonSerializable(typeof(IReadOnlyList<TrainingSample>))]
+internal sealed partial class SourceGenerationContext : JsonSerializerContext
+{
+}
+#pragma warning restore SA1402 // File may only contain a single type
